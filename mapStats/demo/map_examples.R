714a1fbd4ca0884cfaf61dc5842151ccec9dd9f9
sam-data-guy-iam <124508972+sam-data-guy-iam@users.noreply.github.com>
2023-11-06 21:58:18 +0200

    Add files via upload

--- mapStats/demo/map_examples.R
+++ mapStats/demo/map_examples.R
@@ -1,44 +1,10 @@
 #download shapefile
-usMap <- readShapePoly(system.file("shapes/usMap.shp", package="mapStats")[1])
-
-#create synthetic survey dataset
+usMap <- sf::as_Spatial(sf::st_read(system.file("shapes/usMap.shp", package="mapStats")[1]))
 
 
+#create synthetic survey dataset from internal function
 
-state_codes <- c('AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL',
-                 'IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE',
-                 'NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD',
-                 'TN','TX','UT','VT','VA','WA','WV','WI','WY')
-
-surveydata <- data.frame(state=factor(rep(rep(state_codes, 
-                         times=sample(x=c(30,40,60), size=length(state_codes), replace=TRUE)), 
-                         times=2)))
-surveydata$year <- factor(rep(c(2009, 2010), each=nrow(surveydata)/2))
-surveydata$educ <- factor(sample(c("HS","BA","Grad"), p=c(.4,.45,.15), replace=TRUE, size=nrow(surveydata)))
-surveydata$sex <- factor(sample(c("Male","Female"), replace=TRUE, size=nrow(surveydata)))
-
-surveydata$obs_weight <- runif(n=nrow(surveydata), min=0.8, max=1.5)
-
-#two income distributions
-surveydata$income <- 100000*rbeta(n=nrow(surveydata), 
-                                  shape1=ifelse(surveydata$year==2009, 2, 1.5),
-                                  shape2=ifelse(surveydata$year==2010, 10, 11))
-surveydata$income_ge30k <- ifelse(surveydata$income >=30000, 100, 0)
-
-surveydata$age <- round(100*rbeta(n=nrow(surveydata), shape1=2, shape2=4))
-
-#these state and year combinations will not be shaded if they are missing entirely
-surveydata[ surveydata$state == "NV" & surveydata$year == 2009, c("income","income_ge30k")] <- NA
-surveydata[ surveydata$state == "OH" & surveydata$year == 2010, c("income","income_ge30k")] <- NA
-
-#create some missing values in other variables
-surveydata[ sample(1:nrow(surveydata), 15), c("income","income_ge30k")] <- NA
-surveydata[ sample(1:nrow(surveydata), 15), "obs_weight"] <- NA
-surveydata[ sample(1:nrow(surveydata), 15), "year"] <- NA
-surveydata[ sample(1:nrow(surveydata), 15), "state"] <- NA
-
-
-head(surveydata)
+surveydata <- synthetic_US_dataset()
 
 #Calculate weighted mean and 50th, and 60th quantiles of variable income,
 #by state, survey year, and education.  Display using red sequential color palette with 6 groups.
@@ -48,14 +14,16 @@ head(surveydata)
 #print each statistic on a separate page, using separate color breaks for each.  Will print 2 pages.       
 
 
-mapStats(d=surveydata, var="income", stat=c("mean", "quantile"), quantiles=.5, wt.var="obs_weight",
+mapStats(d=surveydata, main.var="income", stat=c("mean", "quantile"), quantiles=.5, wt.var="obs_weight",
                d.geo.var="state", by.var=c("year","educ"), map.file=usMap, map.label=FALSE,
                par.strip.text = list(cex = .7))
+
+
 
 #In the titles, rename 'income' with 'household income', and  'educ' with 'education'.
 #Change the number of color breaks from 4 to 6, add state labels.               
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year","educ"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year","educ"),
          wt.var="obs_weight", map.file=usMap, map.geo.var="state",
          stat=c("mean","quantile"), quantiles=.5,
          paletteName="Reds", ngroups=6, var.pretty="household income",
@@ -68,7 +36,7 @@ mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year","educ"),
 #and use layout argument to position panels within each plot properly .
 #eliminate labels because too crowded.     
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean","quantile"),
          quantiles=c(.25, .5, .75), paletteName="Reds", ngroups=6, 
@@ -80,7 +48,7 @@ mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
 #shrink plot titles so all fit. 
 
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean","quantile"),
          quantiles=c(.25, .5, .75), paletteName="Reds", ngroups=6, 
@@ -91,7 +59,7 @@ mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
 #do the above example, except with a different palette for each to distinguish them 
 #since separate breaks
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean","quantile"),
          quantiles=c(.25, .5, .75), paletteName=c("Reds","Greens","Blues","Greys"), ngroups=6, 
@@ -103,7 +71,7 @@ mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
 
 #use the above with the same palette except with the same color breaks for all statistics,
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean","quantile"),
          quantiles=c(.25, .5, .75), paletteName=c("Reds","Greens","Blues","Greys"), ngroups=6, 
@@ -114,7 +82,7 @@ mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
 
 #do the same except use jenks style to calculate the breaks (classIntervals)
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean","quantile"),
          quantiles=c(.25, .5, .75), paletteName=c("Reds","Greens","Blues","Greys"), ngroups=6, 
@@ -130,7 +98,7 @@ red_hcl <- rev(colorspace::sequential_hcl(n=5, h=0))
 green_hcl <- rev(colorspace::sequential_hcl(n=7, h=120))
 blue_hcl <- rev(colorspace::sequential_hcl(n=3, h=240))
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean","quantile"),
          quantiles=c(.25, .75), colorVec=list(red_hcl, green_hcl, blue_hcl),
@@ -151,7 +119,7 @@ xbox <- new_bounds[1,]
 ybox <- new_bounds[2,]
 
 
-mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
+mapStats(d=surveydata, main.var="income", d.geo.var="state", by.var=c("year"),
          wt.var="obs_weight", map.file=usMap,
          map.geo.var="state", stat=c("mean"), paletteName="Reds", 
          ngroups=6, var.pretty="household income",
@@ -164,7 +132,7 @@ mapStats(d=surveydata, var="income", d.geo.var="state", by.var=c("year"),
 #calculating the percent of respondents by state that have income above 30,000.
 
 
-mapStats(d=surveydata, var="income_ge30k", 
+mapStats(d=surveydata, main.var="income_ge30k", 
          wt.var="obs_weight", map.file=usMap,
          d.geo.var="state", map.geo.var="state",
          stat="mean", paletteName="Reds", 
@@ -174,24 +142,24 @@ mapStats(d=surveydata, var="income_ge30k",
 #Normally you may want to overlay with a different shapefile showing other elements,
 #such as rivers, or something else relevant to the question
 #You can overlay with points to label specific areas on the map, or add a compass point
+#similar examples of sp.layout in https://edzer.github.io/sp/
 
-#subset the map, plot a few green
-state_subset <- usMap[ usMap$state %in% c("TX", "AK", "CA"), ]
-shaded_green <- list("sp.polygons", state_subset, fill="green")
 
-#add a red triangle on the map
+#subset the map, plot a few green with thick blue border; set first=FALSE so gets plotted AFTER the main object
+state_subset <- as(usMap[ usMap$state %in% c("TX", "AK", "CA"), ], "SpatialPolygons")
+shaded_green <- list("sp.polygons", state_subset, fill="green", first=FALSE, col="blue", lwd=4)
+#add a purple triangle on the map
 attributes(usMap)$bbox
 
 rt_coords <- data.frame(x=0, y=2000000)
-coordinates(rt_coords) <- ~ x + y
-red_triangle <- list("sp.points", obj=rt_coords, pch=2, col="red", cex=4, lwd=5)
+sp::coordinates(rt_coords) <- ~ x + y
+purple_triangle <- list("sp.points", obj=rt_coords, pch=2, col="purple", cex=5, lwd=5, first=FALSE)
 
 #combine the two things above in a list
 
-map_overlay <- list(shaded_green, red_triangle)
+map_overlay <- list(shaded_green, purple_triangle)
 
-
-mapStats(d=surveydata, var="income_ge30k", 
+mapStats(d=surveydata, main.var="income_ge30k", 
          wt.var="obs_weight", map.file=usMap,
          d.geo.var="state", map.geo.var="state",
          stat="mean", paletteName="Reds", 
@@ -199,8 +167,7 @@ mapStats(d=surveydata, var="income_ge30k",
          sp_layout.pars=map_overlay)
 
 
-
-#color multiple regions:  create code for Census region
+#color multiple regions:  create code for Census region containing multiple states
 
 west <- c("AK","AZ","WA","MT","OR","ID","WY","CA","NV","UT","CO","NM","HI")
 midwest <- c("ND","MN","SD","NE","IA","WI","MI","OH","IN","IL","MO","KS")
@@ -222,7 +189,7 @@ usMap$region[ usMap$state %in% south ] <- "South"
 #all statistics (separate = 0), and stack vertically
 #multiple statistics and no by variable
 
-mapStats(d=surveydata, var=c("income","age"), wt.var="obs_weight", map.file=usMap,
+mapStats(d=surveydata, main.var=c("income","age"), wt.var="obs_weight", map.file=usMap,
          d.geo.var="region", map.geo.var="region",
          stat=c("mean", "quantile", "var"), quantiles=c(.5),
          paletteName=c("Reds"), ngroups=3, var.pretty=c("household income","age"),
@@ -235,7 +202,7 @@ mapStats(d=surveydata, var=c("income","age"), wt.var="obs_weight", map.file=usMa
 #Greens ignored because only 2 variables and separate=2
 #plotbyvar can be changed
 
-mapStats(d=surveydata, var=c("income","age"), wt.var="obs_weight", map.file=usMap,
+mapStats(d=surveydata, main.var=c("income","age"), wt.var="obs_weight", map.file=usMap,
          d.geo.var="region", map.geo.var="region", by.var=c("year"),
          stat=c("mean", "quantile"), quantiles=c(.25, .75),
          paletteName=c("Reds","Blues","Greens"), ngroups=c(3,4), var.pretty=c("household income","age"),
@@ -248,7 +215,7 @@ mapStats(d=surveydata, var=c("income","age"), wt.var="obs_weight", map.file=usMa
 #so using the same colors is silly but it could
 
 
-mapStats(d=surveydata, var=c("income","age"), wt.var="obs_weight", map.file=usMap,
+mapStats(d=surveydata, main.var=c("income","age"), wt.var="obs_weight", map.file=usMap,
          d.geo.var="region", map.geo.var="region", by.var=c("year"),
          stat=c("mean", "quantile"), quantiles=c(.25, .75),
          paletteName=c("Reds","Blues","Greens"), ngroups=c(3,4), var.pretty=c("household income","age"),
